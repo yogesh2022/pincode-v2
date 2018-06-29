f75e6952316e56879d8d549875a21516ff950d77
Yogesh Sharma <yogesh.sharma85@outlook.com>
2018-06-30 02:53:21 +0530

    Search Functionality Added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Platform } from 'ionic-angular';
 import { ChangeDetectorRef } from "@angular/core";
+import { AboutPage } from '../about/about';
 
 @Component({
   selector: 'page-home',
@@ -10,22 +11,51 @@ import { ChangeDetectorRef } from "@angular/core";
 export class HomePage {
   db : any;
   rowItem = [];
-
+  sqlDB : object = {};
+  pushPage:any;
   constructor(public navCtrl: NavController, platform: Platform, private changeDetectorRef: ChangeDetectorRef) {
     platform.ready().then(() => {
-      this.db = (<any>window).sqlitePlugin.openDatabase({name: "pincode.db", location: 'default', createFromLocation: 1});
-      this.db.executeSql('select * from pincode where pincode=110049', [], (resultSet) => {
-        this.showData.call(this, resultSet);
+      // this.db = (<any>window).sqlitePlugin.openDatabase({name: "pincode.db", location: 'default', createFromLocation: 1});
+      // this.db.executeSql('select * from pincode where pincode=110049', [], (resultSet) => {
+      //   this.showData.call(this, resultSet);
+      // },(error) => {
+      //   console.log('SELECT error: ' + error.message);
+      // });
+      this.pushPage = AboutPage;
+      this.sqlDB = new Promise ((resolve, reject) => {
+        this.db = (<any>window).sqlitePlugin.openDatabase({name: "pincode.db", location: 'default', createFromLocation: 1});
+
+        if(this.db){
+          resolve(this.db);
+        }else{
+          reject('ERROR: DB Connection Failed');
+        }
+      })
+
+    });
+  }
+  getItems(event){
+    // for(var x = 0; x < items.rows.length; x++) {
+    //   this.rowItem.push(items.rows.item(x));
+    // }
+    // console.log(JSON.stringify(this.rowItem));
+    // this.changeDetectorRef.detectChanges();
+    this.rowItem = [];
+    const val = event.target.value;
+    if (val && val.trim() != '' && val.length > 4) {
+      this.db.executeSql('select * from pincode where pincode=?', [val], (resultSet) => {
+        for(var x = 0; x < resultSet.rows.length; x++) {
+          this.rowItem.push(resultSet.rows.item(x));
+        }
+        console.log(JSON.stringify(this.rowItem));
+        this.changeDetectorRef.detectChanges();
       },(error) => {
         console.log('SELECT error: ' + error.message);
       });
-    });
-  }
-  showData(items){
-    for(var x = 0; x < items.rows.length; x++) {
-      this.rowItem.push(items.rows.item(x));
     }
-    console.log(JSON.stringify(this.rowItem));
-    this.changeDetectorRef.detectChanges();
+  }
+
+  itemSelected(item:object) {
+    console.log(item);
   }
 }
